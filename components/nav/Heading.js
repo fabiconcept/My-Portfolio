18544cad5804c4593b910fc8917786e8798571ca
Fabian Ajokubi <favourajokubi@gmail.com>
2024-01-29 15:52:49 +0100

    Update my CV

--- components/nav/Heading.js
+++ components/nav/Heading.js
@@ -6,7 +6,7 @@ export const Heading = () => {
   return (
     <header className={styles.heading}>
       <MyLinks />
-      <OutlineButton onClick={() => window.open("/mycv.pdf")}>
+      <OutlineButton onClick={() => window.open("/fabiconcept_cv.pdf")}>
         My resume
       </OutlineButton>
     </header>
