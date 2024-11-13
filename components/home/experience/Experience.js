ef41d46683858ec584cdd3794d15335f5544821b
fabiconcept <favourajokubi@gmail.com>
2024-11-13 11:39:32 +0100

    Add two new experience entries to the Experience component

--- components/home/experience/Experience.js
+++ components/home/experience/Experience.js
@@ -14,6 +14,35 @@ export const Experience = () => {
 
 const experience = [
   {
+    title: "American International School of Medicine (AISM)",
+    position: "Web Developer",
+    time: "Oct. 2024 - Present",
+    location: "Alpharetta, US (Remote)",
+    description: "As a Web Developer for AISM, I am responsible for enhancing the school's online presence by developing and maintaining a responsive, user-friendly website that supports student engagement and information accessibility. My work involves implementing modern front-end technologies and optimizing the site for performance. I collaborate with the AISM team to ensure the platform is aligned with their goals in delivering a seamless online experience for students, faculty, and prospective students.",
+    tech: [
+      "Next.js",
+      "Tailwind CSS",
+      "Vercel",
+      "Sirv",
+      "React"
+    ]
+  },
+  {
+    title: "Supervision Company",
+    position: "AppSheet Developer",
+    time: "Sept. 2024 - Present",
+    location: "New Jersey, US (Remote)",
+    description: "As an AppSheet Developer at Supervision Company, I develop and manage custom applications to streamline business operations and improve data accessibility across teams. My role involves creating data-driven workflows and interfaces that empower users to interact with complex datasets easily. I work closely with stakeholders to design intuitive solutions that meet specific business needs, leveraging AppSheet's low-code capabilities for rapid development. This includes automating processes, enhancing user experience, and integrating apps seamlessly with Supervision's existing systems.",
+    tech: [
+      "AppSheet",
+      "Google Sheets",
+      "JavaScript",
+      "Google Apps Script",
+      "Google Workspace",
+      "APIs"
+    ]
+  },
+  {
     title: "Cold Hubs Limited",
     position: "Backend Developer",
     time: "Aug. 2024 - Present",
@@ -22,6 +51,7 @@ const experience = [
     tech: [
       "Laravel",
       "PHP",
+      "Figma",
       "MySQL",
       "Git",
       "GitHub",
@@ -46,7 +76,7 @@ const experience = [
     title: "OffrCloud",
     position: "Part-Time Remote Blockchain Developer",
     time: "Dec. 2022 - July. 2023",
-    location: "Atlanta, US",
+    location: "Atlanta, US (Remote)",
     description: "During my time at OffrCloud, I led the development of a cutting-edge FinTech Dapp System as a Lone Remote Blockchain Developer. I not only coded but also created the captivating Landing Page, representing our innovation. My role included designing and securing an ERC20 Security Dividend Token, managing token sales, and deploying a transparent dividend distribution smart contract. These experiences deepened my blockchain knowledge and emphasized the importance of secure financial systems.",
     tech: [
       "React",
