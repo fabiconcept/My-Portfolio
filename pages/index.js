164851b70d6825ef34ce9548d66fb19aab9a4a66
Fabian Ajokubi <70838932+fabiconcept@users.noreply.github.com>
2025-05-20 21:56:33 +0100

    Update favicon

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function home() {
         <title>Favour Ajokubi | Web Developer</title>
         <meta name="description" content="Hey i'm Favour Tochukwu Ajokubi and i'm a developer" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicons/favicon.ico" />
       </Head>
       <Home />
     </>
