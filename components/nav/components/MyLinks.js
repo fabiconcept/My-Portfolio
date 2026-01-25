0455755e60534786127eb3335e962d3ae3abfa27
fabiconcept <favourajokubi@gmail.com>
2026-01-25 02:02:05 +0100

    Add Dribbble social link and improve accessibility with title attributes
    - Import AiOutlineDribbble icon from react-icons/ai
    - Add new Dribbble social link with motion animation (0.3s delay)
    - Add title attributes to all social link spans for better accessibility (Linkedin, Github, Twitter, Instagram, Dribbble)
    - Update LinkedIn URL from full profile path to shortened /in/fabiconcept format

--- components/nav/components/MyLinks.js
+++ components/nav/components/MyLinks.js
@@ -5,6 +5,7 @@ import {
   AiFillTwitterCircle,
   AiFillCodepenCircle,
   AiFillInstagram,
+  AiOutlineDribbble,
 } from "react-icons/ai";
 import Link from "next/link";
 import { motion } from "framer-motion";
@@ -16,8 +17,9 @@ export const MyLinks = () => {
         initial={{ y: -30, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.5, delay: 0.0 }}
+        title="Linkedin"
       >
-        <Link href="https://www.linkedin.com/in/favour-ajokubi-371904222/" target="_blank" rel="nofollow"> 
+        <Link href="https://www.linkedin.com/in/fabiconcept" target="_blank" rel="nofollow"> 
           <AiFillLinkedin size="2.4rem" />
         </Link>
       </motion.span>
@@ -26,6 +28,7 @@ export const MyLinks = () => {
         initial={{ y: -30, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.5, delay: 0.1 }}
+        title="Github"
       >
         <Link href="https://www.github.com/fabiconcept" target="_blank" rel="nofollow">
           <AiFillGithub size="2.4rem" />
@@ -36,6 +39,7 @@ export const MyLinks = () => {
         initial={{ y: -30, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.5, delay: 0.2 }}
+        title="Twitter"
       >
         <Link href="https://www.twitter.com/goath2o" target="_blank" rel="nofollow">
           <AiFillTwitterCircle size="2.4rem" />
@@ -46,9 +50,21 @@ export const MyLinks = () => {
         initial={{ y: -30, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.5, delay: 0.3 }}
+        title="Instagram"
       >
         <Link href="https://www.instagram.com/fabiconcept_ng" target="_blank" rel="nofollow">
           <AiFillInstagram size="2.4rem" />
+        </Link>
+      </motion.span>
+
+      <motion.span
+        initial={{ y: -30, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.3 }}
+        title="Dribbble"
+      >
+        <Link href="https://dribbble.com/fabiconcept" target="_blank" rel="nofollow">
+          <AiOutlineDribbble size="2.4rem" />
         </Link>
       </motion.span>
     </div>
